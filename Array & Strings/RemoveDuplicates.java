83237178b681904b2b03882eb2c0702e3315776a
BettaibiMedAli <mohamedalibettaibi22@gmail.com>
2024-10-13 12:26:48 +0200

    Added solution for remove duplicates problem

---
+++ Array & Strings/RemoveDuplicates.java
@@ -0,0 +1,22 @@
+import java.util.*;
+class Solution {
+    public int removeDuplicates(int[] nums) {
+        ArrayList<Integer> tab = new ArrayList<>();
+        int min = nums[0];
+        tab.add(0);
+        for(int i = 1; i < nums.length ; i++)
+        {
+            if(nums[i] != min)
+            {
+                tab.add(i);
+                min = nums[i];
+            }
+        }
+        int n = tab.size();
+        for(int i = 0; i < n ; i++)
+        {
+            nums[i] = nums[tab.get(i)];
+        }
+        return n;
+    }
+}
