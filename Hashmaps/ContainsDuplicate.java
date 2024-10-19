af1482a4005e14ad3fe2afbeef091e2fa5fd22e5
BettaibiMedAli <mohamedalibettaibi22@gmail.com>
2024-10-19 12:00:10 +0200

    Added solution for contains duplicate problem

---
+++ Hashmaps/ContainsDuplicate.java
@@ -0,0 +1,13 @@
+package Hashmaps;
+import java.util.*;
+
+public class ContainsDuplicate {
+    public boolean containsDuplicate(int[] nums) {
+        HashSet<Integer> set = new HashSet<>();
+        for(int i : nums)
+        {
+            if(set.add(i) == false) return true;
+    }
+    return false;
+    }
+}
